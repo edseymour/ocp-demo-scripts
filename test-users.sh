008cacbdfc2a0b73b7ac585208c3e93aa6ec1a7d
edseymour <eseymour@redhat.com>
2016-10-13 15:28:31 +0100

    added user test

---
+++ test-users.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+function check_exists()
+{
+  [[ "$2" == "" ]] && echo "Missing $1" && exit 1
+}
+
+function default_if_empty()
+{
+  if [[ "$2" == "" ]]; then 
+    echo $1
+  else
+    echo $2
+  fi
+}
+
+while IFS=, read user password name
+do
+
+oc project dev-$user
+
+if [[ "dev-$user" == "$(oc project -q)" ]]
+then
+
+oc delete all --all
+oc new-app monster
+
+while [[ $(oc get builds --no-headers | wc -l) -lt 1 ]]
+do
+   sleep 1
+done
+
+oc logs -f builds/monster-1
+sleep 1
+oc logs -f monster-1-deploy
+
+fi
+
+oc project uat-$user
+if [[ "uat-$user" == "$(oc project -q)" ]]
+then
+
+oc delete all --all 
+oc new-app monster-app
+
+sleep 1
+oc logs -f monster-mysql-1-deploy
+oc tag monster:latest monster:uat -n dev-$user
+
+oc logs -f monster-1-deploy
+
+fi
+
+for attempt in $(seq 1 10); do
+ ret=$(curl -Is http://monster-dev-$user.apps.openshift.red | grep HTTP| awk '{print $2}')
+ [[ $ret -eq 200 ]] && echo "*** dev-$user SUCCESS" && break
+ echo "*** dev-$user attempt#$attempt"
+done
+
+for attempt in $(seq 1 10); do
+ ret=$(curl -Is http://monster-uat-$user.apps.openshift.red | grep HTTP| awk '{print $2}')
+ [[ $ret -eq 200 ]] && echo "*** dev-$user SUCCESS" && break
+ echo "*** dev-$user attempt#$attempt"
+done
+
+oc delete all --all -n dev-$user
+oc delete all --all -n uat-$user
+
+
+done <users.csv
