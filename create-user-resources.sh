61fa4e9b5424dd83d7c843f7039928feed66785f
edseymour <eseymour@redhat.com>
2016-10-13 14:04:47 +0100

    fixed user creation script

--- create-user-resources.sh
+++ create-user-resources.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-#Gogs ref
-#https://github.com/gogits/go-gogs-client/wiki/Repositories
-
-#Creating a repo
-#curl -v -u demo02:r3dh4t* -H "Content-Type: application/json" -X POST -d '{"name":"hello","description":"Some name","private":false}'  http://gogs.apps.openshift.red/api/v1/user/repos
-#Create user
-#{
-#    "source_id": 1,
-#    "login_name": "apiuser",
-#    "username": "apiuser",
-#    "email": "apiuser@user.com"
-#}
-
 #      ref: master
 #      uri: https://github.com/jim-minter/ose3-demos.git
 
@@ -45,14 +32,11 @@ git config credential.helper 'store'
 while IFS=, read user password name
 do
 
-#USERADD='{ "source_id": 1, "login_name": "'$user'", "username": "'$user'", "email": "'$user'@openshift.red","password":"'$password'" }'
-#REPOADD='{"name":"monster","description":"Ticket Monster for '$name'","private":false}'
-
 ## add the user
-curl -v -u $GOGSUSER:$GOGSPASS -H "Content-Type: application/json" -X POST -d '{ "source_id": 1, "login_name": "'$user'", "username": "'$user'", "email": "'$user'@openshift.red","password":"'$password'" }' $GOGSURL/api/v1/admin/users
+curl -v -u $GOGSUSER:$GOGSPASS -H "Content-Type: application/json" -X POST -d '{ "username": "'"$user"'", "email": "'"$user"'@openshift.red","password":"'"$password"'" }' $GOGSURL/api/v1/admin/users
 
 ## create the repo
-curl -v -u $user:$password -H "Content-Type: application/json" -X POST -d '{"name":"monster","description":"Ticket Monster for '$name'","private":false}' $GOGSURL/api/v1/user/repo
+curl -v -u $user:$password -H "Content-Type: application/json" -X POST -d '{"name":"monster","description":"Ticket Monster for '"$name"'","private":false}' $GOGSURL/api/v1/user/repo
 
 echo "http://$user:$password@gogs.apps.openshift.red" > $HOME/.git-credentials
 
