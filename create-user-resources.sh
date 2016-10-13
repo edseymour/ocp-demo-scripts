a6f9bf75bd7434c00bf691d5f09b8dc74af2010e
edseymour <eseymour@redhat.com>
2016-10-13 12:40:03 +0100

    initial version of create-user-resource automation

--- create-user-resources.sh
+++ create-user-resources.sh
@@ -27,12 +27,51 @@ function default_if_empty
 }
 
 
-USAGE="$0 <number of users>"
+USAGE="$0 <Gogs URL> <Gogs User> <Gogs Password>"
 CLONE_URL=https://github.com/jim-minter/ose3-demos.git
 DOWNLOAD_URL=https://github.com/jim-minter/ose3-demos/archive/master.zip
 
-NUM=$1
-check_exists "number of users to create, $USAGE" $NUM
+wget $DOWNLOAD_URL
+unzip master.zip
 
-GOGS_URL=$1
+GOGSURL=$1
+check_exists "provide a Gogs URL, $USAGE" $GOGSURL
+
+GOGSUSER=$2
+check_exists "provide a Gogs user, $USAGE" $GOGSUSER
+GOGSPASS=$3
+check_exists "provide a Gogs user password, $USAGE" $GOGSPASS
+
+git config credential.helper 'store'
+
+while IFS=, read user password name
+do
+
+USERADD='{ "source_id": 1, "login_name": "'$user'", "username": "'$user'", "email": "'$user'@openshift.red","password":"'$password'" }'
+REPOADD='{"name":"monster","description":"Ticket Monster for '$name'","private":false}'
+
+## add the user
+curl -v -u $GOGSUSER:$GOGSPASS -H "Content-Type: application/json" -X POST -d $USERADD $GOGSURL/api/v1/admin/users
+
+## create the repo
+curl -v -u $user:$password -H "Content-Type: application/json" -X POST -d $REPOADD $GOGSURL/api/v1/user/repo
+
+echo "http://$user:$password@gogs.apps.openshift.red" > $HOME/.git-credentials
+
+pushd ose3-demos-master/git/monster/
+
+git init
+git add .
+git commit -am "inital commit for $name"
+git remote add origin $GOGSURL/$user/monster.git
+git push -u origin master
+
+# clean git init
+rm -rf .git
+
+popd
+
+
+done <users.csv
+
 
