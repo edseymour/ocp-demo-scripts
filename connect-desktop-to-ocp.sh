eae8e1fdb12f0daa7373153927ef9c2540e83d04
Ed Seymour <eseymour@redhat.com>
2017-01-23 07:52:22 -0500

    fixed connect-desktop script

--- connect-desktop-to-ocp.sh
+++ connect-desktop-to-ocp.sh
@@ -17,9 +17,12 @@ function default_if_empty
 USAGE="$0 <home-dir> <Gogs URL> <Gogs User> <Gogs Password>"
 
 HOMED=$1
-check_exists "provide a home directory, $USAGE" $HOMED
+check_exists "provide a base home directory, $USAGE" $HOMED
 GOGSURL=$2
 check_exists "provide a Gogs URL, $USAGE" $GOGSURL
+GOGSHOST=$(echo $GOGSURL | awk -F/ '{print $3}')
+GOGSSCHEME=$(echo $GOGSURL | awk -F/ '{print $1}')
+echo "Using $GOGSSCHEME//$GOGSHOST as the Gogs base URL"
 GOGSUSER=$3
 check_exists "provide a Gogs user, $USAGE" $GOGSUSER
 GOGSPASS=$4
@@ -34,18 +37,19 @@ curl -v -u $GOGSUSER:$GOGSPASS -H "Content-Type: application/json" -X POST -d '{
 ## create the repo
 curl -v -u $user:$password -H "Content-Type: application/json" -X POST -d '{"name":"monster","description":"Ticket Monster for '"$name"'","private":false}' $GOGSURL/api/v1/user/repos
 
+USERHOME=$HOMED/$user
 
-runuser -l $user -c "pushd $HOMED/code/monster
+runuser -l $user -c "pushd $USERHOME/code/monster
 git init
 git add .
 git config --global user.name \"$name\"
 git config --global user.email \"$user@openshift.red\"
 git config credential.helper 'store --file .git/credentials'
-echo \"http://$user:$password@gogs.apps.openshift.red\" > .git/credentials
+echo \"$GOGSSCHEME//$user:$password@$GOGSHOST\" > .git/credentials
 chmod 600 .git/credentials
 
 git commit -am \"inital commit for $name\"
-git remote add origin $GOGSURL/$user/monster.gi
+git remote add origin $GOGSURL/$user/monster.git
 git push -u origin master
 
 popd"
