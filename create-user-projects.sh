f4f7ba21ba3a31713fe12e6b3ff08a62068d7294
Hackathon User <coder1@openshift.red>
2017-10-15 16:45:09 +0100

    inital commit for Hackathon User

--- create-user-projects.sh
+++ create-user-projects.sh
@@ -29,11 +29,12 @@ do
 echo "**"
 echo "**"
 echo "** Creating default projects for $user"
-oc adm new-project dev-$user --display-name="App Dev - $name ($user)" --description="Application development project, where applications are coded and built" --admin=$user
-oc adm new-project uat-$user --display-name="App Test - $name ($user)" --description="Application testing project, where applications tested and approved" --admin=$user
-oc adm new-project prod-$user --display-name="App Prod - $name ($user)" --description="Application production project, where applications are hosted" --admin=$user
+oc adm new-project dev-$user --display-name="1. JEE Dev - $name ($user)" --description="Example JEE application development project, where applications are coded and built" --admin=$user
+oc adm new-project uat-$user --display-name="2. JEE Test - $name ($user)" --description="Example JEE application testing project, where applications tested and approved" --admin=$user
+oc adm new-project prod-$user --display-name="3. JEE Prod - $name ($user)" --description="Example JEE application production project, where applications are hosted" --admin=$user
 
 sed 's|%GITURL%|'"$GOGSURL/$user"'/monster.git|g' monster-dev.yaml | sed 's|%MAVENURL%|'"$MAVENURL"'|g' | oc create -n dev-$user -f -
+sed 's|%GITURL%|'"$GOGSURL/$user"'/monster.git|g' monster-build.yaml | sed 's|%MAVENURL%|'"$MAVENURL"'|g' | oc create -n dev-$user -f -
 sed 's/%DEVNAMESPACE%/'"dev-$user"'/g' monster-test.yaml | oc create -n uat-$user -f -
 sed 's/%DEVNAMESPACE%/'"dev-$user"'/g' monster-prod.yaml | oc create -n prod-$user -f -
 sed 's/%APP/monster/g' pipeline-template.yaml | sed 's/%DEV_PROJ/dev-'"$user"'/g' | sed 's/%TEST_PROJ/uat-'"$user"'/g' | sed 's/%PROD_PROJ/prod-'"$user"'/g' | oc create -n dev-$user -f -
